fab01ff56e84ed98136afff58c2a3a0a266e518b
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-03 17:20:59 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser="Chris Evans"
+ let welcomeMessage="Welcome to Flatbook, + Chris Evans !"
+  let excitedWelcomeMessage = "WELCOME TO FLATBOOK,  + CHRIS EVANS!"
+  const shortGreeting="Welcome, C!"
+  
+
+
+
+
+ 
